c93fdac5b24d1b2bda1a5d5dbb7a69b7a897ee53
Chris Heaps <lisachrisandgirls@googlemail.com>
2026-03-20 09:51:22 +0000

    Added AppDbContextFactory to deal with GitHub workflow build error.

---
+++ DeckDuelAPI/Data/AppDbContextFactory.cs
@@ -0,0 +1,24 @@
+using DeckDuel2.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace DeckDuel2.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
