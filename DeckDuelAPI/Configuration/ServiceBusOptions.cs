9b8e475e88292d93febda8e2bedcd9e98127e5f1
Chris Heaps <lisachrisandgirls@googlemail.com>
2026-04-09 22:26:08 +0100

    Removed service bus and added GameTurnLockProvider in its place. refs #21

--- DeckDuelAPI/Configuration/ServiceBusOptions.cs
+++
@@ -1,9 +0,0 @@
-namespace DeckDuel2.Configuration
-{
-    public class ServiceBusOptions
-    {
-        public const string SectionName = "ServiceBus";
-        public string ConnectionString { get; set; } = string.Empty;
-        public string QueueName { get; set; } = string.Empty;
-    }
-}
